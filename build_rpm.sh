be33c582651320cb786a521fa30aa31e10a73a62
Yanis Guenane <yguenane@redhat.com>
2017-04-27 09:44:01 +0200

    Add ansible-role-openstack-stackdump and ansible-role-openstack-certification as non_py_projects
    Change-Id: I951dd7b418cfb31abbc3b0f557c0b2800fc11745

--- build_rpm.sh
+++ build_rpm.sh
@@ -146,7 +146,7 @@ if [[ "$PROJ_NAME" == "dci-gpgpubkey" ]]; then
     cp distributed-ci.pub ${HOME}/rpmbuild/SOURCES/
 fi
 
-non_py_projects=("dci-ansible", "dci-ansible-agent", "dci-ui", "ansible-role-dci-feeders")
+non_py_projects=("dci-ansible", "dci-ansible-agent", "dci-ui", "ansible-role-dci-feeders", "ansible-role-openstack-stackdump", "ansible-role-openstack-certification")
 if [[ -e setup.py ]]; then
     DATE=$(date +%Y%m%d%H%M)
     SHA=$(git rev-parse HEAD | cut -c1-8)
