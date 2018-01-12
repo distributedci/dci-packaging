11a2d7aac293e94c335092c96d97ef11740448c9
Tristan Cacqueray <tdecacqu@redhat.com>
2018-01-12 04:14:15 +0000

    Add sign_rpm.sh script
    Change-Id: Ia23ffae9340deb8c13bf4c521be8708d63629bc4

---
+++ sign_rpm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+if [[ "$#" -lt 1 ]]; then
+    echo "USAGE: $0 [<PATH_TO_RPM>]"
+    exit 1
+fi
+
+echo '%_signature gpg' > ~/.rpmmacros
+echo '%_gpg_name Distributed-CI' >> ~/.rpmmacros
+
+# Unlock gpg key
+while (( "$#" )); do
+    PKG="$1"
+    [ -f "$PKG" ] || { echo "$PKG: not a file"; exit 1; }
+    echo "Signing ${PKG}"
+    expect <<EOF
+set timeout 300
+spawn rpm --addsign ${PKG}
+expect "Enter pass phrase: "
+send -- "\r"
+expect eof
+EOF
+    shift
+done
