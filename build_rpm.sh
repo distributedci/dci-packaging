721574594f182678a62fd84654e7fed1d075a3eb
Haikel Guemar <hguemar@fedoraproject.org>
2020-04-30 10:23:48 +0200

    Make build_rpm.sh build_target configurable
    - if no parameter is set, default is epel-7-x86_64
    - generate_mock_profile_v2: externalize build target configuration
    - simplify mock parameters management
    - Use pristine location for RDO repo GPG key
    
    Change-Id: I0e5155cd69fa401e3af7ef2d88783fa2fb8f2b6a

--- build_rpm.sh
+++ build_rpm.sh
@@ -10,38 +10,34 @@ fi
 
 PATH_TO_PROJ=$1
 PROJ_NAME=$2
-if [[ "$#" == 3 ]]; then
-    PATH_TO_REPO=$3
+if [[ "$#" == 4 ]]; then
+    PATH_TO_REPO=$4
 else
     PATH_TO_REPO=""
 fi
 
 WORKSPACE='current'
-SUPPORTED_DISTRIBUTIONS='epel-7-x86_64'
 RDO_CLOUD_MIRROR='mirror.regionone.rdo-cloud.rdoproject.org'
 
-pushd ${PATH_TO_PROJ}
-
-arch="epel-7-x86_64"
+arch="${3:-epel-7-x86_64}"
 rpath=$(echo ${arch}|sed s,-,/,g|sed 's,epel,el,')
 with_args=""
+basedir=$PWD
+
+MOCKOPTS="-r ${HOME}/.mock/${arch}-with-extras.cfg"
 
 generate_mock_profile
 setup_build
-mock -r ${HOME}/.mock/${arch}-with-extras.cfg --init
+mock $MOCKOPTS --init
+
+pushd ${PATH_TO_PROJ}
+
 generate_srpm
 # Use a TTL=4 to evaluate the distance between the host the mirror
 ping -c 2 -t 4 -W 1 ${RDO_CLOUD_MIRROR} && set_rdo_cloud_mirror ${HOME}/.mock/${arch}-with-extras.cfg
-
-if [[ "$PROJ_NAME" == "dci-control-server" ]]; then
-    with_args="--enablerepo centos-openstack-rocky --enablerepo centos-sclo-rh --enablerepo dci-extras"
-elif [[ "$PROJ_NAME" == "python-dciclient" ]] || [[ "$PROJ_NAME" == "dci-ui" ]]; then
-    with_args="--enablerepo centos-sclo-rh"
-fi
-
+setup_additional_repos
 # Build the RPMs in a clean chroot environment with mock to detect missing
 # BuildRequires lines.
-mock -r ${HOME}/.mock/${arch}-with-extras.cfg rebuild ${with_args} --resultdir=${WORKSPACE}/${rpath} ${HOME}/rpmbuild/SRPMS/*.src.rpm 2>&1
-
+mock $MOCKOPTS rebuild --resultdir=${WORKSPACE}/${rpath} ${HOME}/rpmbuild/SRPMS/*.src.rpm 2>&1
 
 popd
