dcdcf996f846db96819733903255bc4e9d2de3fc
Guillaume Vincent <gvincent@redhat.com>
2023-10-20 09:57:31 +0000

    Add new GPG signature to the RPM
    Change-Id: I94fb8b0d65128646a4e69ae18d10f8788787dcd3
    Depends-On: https://softwarefactory-project.io/r/c/dci-config/+/29828
    Depends-On: https://softwarefactory-project.io/r/c/dci-release/+/29838

--- sign_rpm.sh
+++ sign_rpm.sh
@@ -5,6 +5,13 @@ if [[ "$#" -lt 1 ]]; then
     exit 1
 fi
 
+add_sign_to_rpm () {
+    echo "Signing the rpm with the new key '$1'"
+    echo '%_signature gpg' > ~/.rpmmacros
+    echo "%_gpg_name $1" >> ~/.rpmmacros
+
+    rpm --addsign $2
+}
 
 # Unlock gpg key
 while (( "$#" )); do
@@ -12,20 +19,8 @@ while (( "$#" )); do
     [ -f "$PKG" ] || { echo "$PKG: not a file"; exit 1; }
     echo "Signing ${PKG}"
 
-    set +e
-    rpm -qi "${PKG}" | egrep -qs "Release\s+:.*\.el9$"
-    if [ $? -eq 0 ]; then
-        KEY="Distributed-CI EL9 <distributed-ci@redhat.com>"
-    else
-        KEY="Distributed-CI <distributed-ci@redhat.com>"
-    fi
-    set -e
-
-    echo "Using key '${KEY}'"
-    echo '%_signature gpg' > ~/.rpmmacros
-    echo "%_gpg_name ${KEY}" >> ~/.rpmmacros
-
-    rpm --addsign ${PKG}
+    NEW_KEY="Distributed-CI 2024 <distributed-ci@redhat.com>"
+    add_sign_to_rpm "${NEW_KEY}" "${PKG}"
 
     shift
 done
