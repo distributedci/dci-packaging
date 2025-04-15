685e1c324d062f8549812d2cb0add8d039f0b673
François Charlier <francois.charlier@redhat.com>
2025-04-15 17:26:53 +0200

    Create a container image to run createrepo with s3fs
    Change-Id: I04f17000c5edbf9499ccc126eacc2557d95ebb95
    Depends-On: https://softwarefactory-project.io/r/c/dci-config/+/33586

---
+++ createrepo_s3fs.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ -z "${AWS_S3_PACKAGES_MOUNT}" -o -z "${AWS_S3_PACKAGES_BUCKET}"]; then
+  echo "AWS_S3_PACKAGES_MOUNT and AWS_S3_PACKAGES_BUCKET environment variables must be set."
+  exit 1
+fi
+
+test -d "${AWS_S3_PACKAGES_MOUNT}" || mkdir -p "${AWS_S3_PACKAGES_MOUNT}"
+
+s3fs "${AWS_S3_PACKAGES_BUCKET}" "${AWS_S3_PACKAGES_MOUNT}"
+for REPO in "${AWS_S3_PACKAGES_MOUNT}/repos/current/el/*/*"; do
+  repomanage --old --keep 2 ${REPO} | while read OLDRPM; do
+    rm -vf "${OLDRPM}"
+  done
+
+  createrepo --update ${REPO}/
+done
