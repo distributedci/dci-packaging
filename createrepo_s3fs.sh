44d54875031545ad1a6346930ca0a67f3338fa61
François Charlier <francois.charlier@redhat.com>
2025-04-18 12:57:33 +0200

    Fix createrepo_s3fs script typos
    Change-Id: If110786c66d999ad9df8fb6b89d7f0fd9e2df308

--- createrepo_s3fs.sh
+++ createrepo_s3fs.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -z "${AWS_S3_PACKAGES_MOUNT}" -o -z "${AWS_S3_PACKAGES_BUCKET}"]; then
+if [ -z "${AWS_S3_PACKAGES_MOUNT}" -o -z "${AWS_S3_PACKAGES_BUCKET}" ]; then
   echo "AWS_S3_PACKAGES_MOUNT and AWS_S3_PACKAGES_BUCKET environment variables must be set."
   exit 1
 fi
@@ -9,9 +9,10 @@ test -d "${AWS_S3_PACKAGES_MOUNT}" || mkdir -p "${AWS_S3_PACKAGES_MOUNT}"
 
 s3fs "${AWS_S3_PACKAGES_BUCKET}" "${AWS_S3_PACKAGES_MOUNT}"
 for REPO in "${AWS_S3_PACKAGES_MOUNT}/repos/current/el/*/*"; do
-  repomanage --old --keep 2 ${REPO} | while read OLDRPM; do
+  echo ".:[ Processing ${REPO} ]:."
+  repomanage --old --keep 2 "${REPO}" | while read OLDRPM; do
     rm -vf "${OLDRPM}"
   done
 
-  createrepo --update ${REPO}/
+  createrepo --local-sqlite --update "${REPO}/"
 done
